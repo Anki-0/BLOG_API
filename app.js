7b64a12240398db7bd62f9ad05f7e6a29e40eb25
Ankit Singh <singhankit25apr@gmail.com>
2021-07-20 13:23:03 +0530

    .

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 app.use(
  cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+  origin: '*',
   optionsSuccessStatus: 200,
   credentials: true,
  })
