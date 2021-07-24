a36afe06e277748026d1952f84625e8496b7562e
Ankit Singh <singhankit25apr@gmail.com>
2021-07-24 16:12:57 +0530

    ,

--- config/email.js
+++ config/email.js
@@ -3,10 +3,10 @@ const nodemailer = require('nodemailer');
 const sendEmail = async (options) => {
  //1. create a transporter
  const transporter = nodemailer.createTransport({
-  service: process.env.EMAIL_HOST,
+  service: 'gmail',
   auth: {
-   user: process.env.EMAIL_USERNAME,
-   pass: process.env.EMAIL_PASSWORD,
+   user: 'nullbyte08@gmail.com',
+   pass: 'singhankit25apr@9599344675',
   },
  });
 
